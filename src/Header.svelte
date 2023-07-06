a7c7ba3f9505b40275c7fe8446cd830adc4f72f5
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-07-06 04:55:01 -0300

    Update Header.svelte

--- src/Header.svelte
+++ src/Header.svelte
@@ -8,11 +8,11 @@
 </script>
 
 <header>
-<h1>🔫💬</h1>
+<h1>💱</h1>
   {#if $username}
     <div class="user-bio">
       
-      <span>Hello <strong>{$username}</strong></span>
+      <span>Welcome to Club X-Chain <strong>{$username}</strong></span>
       <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
     </div>
 
@@ -20,6 +20,6 @@
 
     {:else}
 
-      <h3>Gun.js Chat</h3>
+      <h3>X-Chain Chat</h3>
   {/if}
 </header>
